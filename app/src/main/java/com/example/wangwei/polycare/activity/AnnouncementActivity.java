e182d2e39b7b39b5184472f32a852632cd184be5
wangwei0118 <jeesy0118@gmail.com>
2018-04-22 15:58:36 +0200

    add scroll  view

--- app/src/main/java/com/example/wangwei/polycare/activity/AnnouncementActivity.java
+++ app/src/main/java/com/example/wangwei/polycare/activity/AnnouncementActivity.java
@@ -1,8 +1,11 @@
 package com.example.wangwei.polycare.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -14,6 +17,17 @@ public class AnnouncementActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_announcement);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(AnnouncementActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }

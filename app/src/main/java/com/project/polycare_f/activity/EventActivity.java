cbbd3946de58b48699d45dace195a629053ab4f7
wangwei0118 <jeesy0118@gmail.com>
2018-05-08 11:41:02 +0200

    add the fonction "Phone someone"

--- app/src/main/java/com/project/polycare_f/activity/EventActivity.java
+++ app/src/main/java/com/project/polycare_f/activity/EventActivity.java
@@ -1,12 +1,21 @@
 package com.project.polycare_f.activity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.polycare_f.R;
 
@@ -24,10 +33,9 @@ public class EventActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
             setContentView(R.layout.event_item_land);
-        }
-        else {
+        } else {
             setContentView(R.layout.event_item);
         }
 
@@ -49,46 +57,75 @@ public class EventActivity extends AppCompatActivity {
         description = intent.getExtras().getString("Description");
         importance = intent.getExtras().getString("Importance");
         date = intent.getExtras().getString("Date");
-        state =intent.getExtras().getString("State");
+        state = intent.getExtras().getString("State");
         phonenumber = intent.getExtras().getString("Phone");
 
 
         arthorview.setText("Arthor : " + prenom);
         titleview.setText("Titre : " + title);
-        importanceview.setText("Urgence : " +importance);
+        importanceview.setText("Urgence : " + importance);
         categoryview.setText("Catégorie : " + category);
         dateview.setText("Date : " + date);
         descriptionview.setText("Description : " + description);
-        stateview.setText( "État : " + state);
+        stateview.setText("État : " + state);
         numberview.setText("Téléphone : " + phonenumber);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.top_menu, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch (item.getItemId()){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
             case R.id.modifier:
                 Intent intent = new Intent(this, ChangeEventActivity.class);
                 intent.putExtra("Title", title);
                 intent.putExtra("Name", prenom);
                 intent.putExtra("Category", category);
                 intent.putExtra("Importance", importance);
-                intent.putExtra("Description",description);
+                intent.putExtra("Description", description);
                 intent.putExtra("State", state);
                 intent.putExtra("Phone", phonenumber);
 
                 this.startActivity(intent);
                 break;
             case R.id.rapeler:
+                phone();
                 break;
         }
         return true;
+    }
+
+    public void phone() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.ic_call_black_24dp);//设置图标
+        builder.setTitle("Validation");//设置对话框的标题
+        builder.setMessage("Vous voulez appele " + phonenumber + " ? ");//设置对话框的内容
+        builder.setPositiveButton("Appeler", new DialogInterface.OnClickListener() {  //这个是设置确定按钮
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:"+phonenumber));
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {  //取消按钮
+
+            @Override
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(EventActivity.this, "Annuler", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        AlertDialog b = builder.create();
+        b.show();  //必须show一下才能看到对话框，跟Toast一样的道理
+
     }
 
 }

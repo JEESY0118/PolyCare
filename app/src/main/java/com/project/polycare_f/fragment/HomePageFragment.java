9ed248b5bf463a798eca5c309703c1ee339d3cb8
wangwei0118 <jeesy0118@gmail.com>
2018-05-17 21:32:52 +0200

    change color of topMenu, and added icon

--- app/src/main/java/com/project/polycare_f/fragment/HomePageFragment.java
+++ app/src/main/java/com/project/polycare_f/fragment/HomePageFragment.java
@@ -1,20 +1,26 @@
 package com.project.polycare_f.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.AttributeSet;
 import android.view.Gravity;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -35,6 +41,7 @@ import com.project.polycare_f.data.Event;
 import com.project.polycare_f.adapter.RecyclerViewAdapter;
 import com.project.polycare_f.interfaces.myItemMoveCallBack;
 
+import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -285,6 +292,21 @@ public class  HomePageFragment extends Fragment {
         return true;
     }
 
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        if (menu != null) {
+            if (menu.getClass() == MenuBuilder.class) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                }
+            }
+        }
+        super.onPrepareOptionsMenu(menu);
+    }
 }
 
 
